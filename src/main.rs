d848d5fb4869cc4fdf449ed4332c2a2d99134293
Amin Bista <aminbista6666@gmail.com>
2024-02-18 21:17:37 +0545

    feat: library crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
+use minigrep::{self, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,28 +8,12 @@ fn main() {
         println!("Problem parsing args: {err}");
         process::exit(1);
     });    
-
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
     println!("Search for: {}", config.query);
-    println!("Search in:\n{contents}");
-}
+    println!("Search in: {}", config.file_path);
 
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough parameters");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Self {query, file_path})
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
+
