a5ebebbdc1b09c39bd0a34c2982a4e060511022b
Amin Bista <aminbista6666@gmail.com>
2024-02-18 21:18:31 +0545

    styling

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 use std::fs;
 use std::error::Error;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub file_path: String
@@ -24,4 +17,11 @@ impl Config {
     
         Ok(Self {query, file_path})
     }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
