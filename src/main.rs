96db9b9969473f8e85ab60a7c73f5257c043da5e
Amin Bista <aminbista6666@gmail.com>
2024-02-18 15:53:38 +0545

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing args: {err}");
+        process::exit(1);
+    });    
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("Search for: {}", config.query);
+    println!("Search in:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough parameters");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(Self {query, file_path})
+    }
 }
